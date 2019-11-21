4abb03955a1e3264fadbd2c86d52662c04661761
ShrishailSGajbhar <shrishailgajbhar5@gmail.com>
2019-11-21 23:37:41 +0530

    Initial Commit

---
+++ HelloKDevelop/build/CMakeFiles/hellokdevelop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellokdevelop.dir/main.cpp.o"
+  "hellokdevelop.pdb"
+  "hellokdevelop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellokdevelop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Template_Study_1/build/CMakeFiles/template_study_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_study_1.dir/main.cpp.o"
+  "template_study_1.pdb"
+  "template_study_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_study_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
